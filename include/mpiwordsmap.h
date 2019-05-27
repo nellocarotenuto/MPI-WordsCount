e2326e5d114ea9b5855e0ec9eeb6a829457e5a33
Nello Carotenuto <nello.carotenuto.96@live.com>
2019-05-27 19:16:58 +0200

    MPI: WordsMap: add comments to the header

--- include/mpiwordsmap.h
+++ include/mpiwordsmap.h
@@ -1,3 +1,9 @@
+/*
+ * Create a datatype for the word count structure that MPI is aware of.
+ */
 void create_type_word(MPI_Datatype *word);
 
+/*
+ * Create a buffer of contiguous word counts to be used in MPI communications.
+ */
 word_node *create_word_list_buffer(int list_length, word_node *list);
