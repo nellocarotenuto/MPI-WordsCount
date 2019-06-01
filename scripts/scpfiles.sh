9c9d121d3472413f1b369ebf651758eb0fe81673
Nello Carotenuto <nello.carotenuto.96@live.com>
2019-06-01 19:43:11 +0200

    Scripts: scpfiles: use bash and avoid host key checking

--- scripts/scpfiles.sh
+++ scripts/scpfiles.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script automates the process of copying the needed files onto the workers.
 #
@@ -7,7 +7,7 @@
 #
 # This script requires your master node to be already capable of connecting to each worker through SSH without password.
 
-if [ "$#" -lt 2 ]
+if [[ "$#" -lt 2 ]]
 then
 	echo "Usage: $0 --hostfile <filename> [--user <username>]"
 	exit
@@ -18,27 +18,27 @@ DATADIR="data/"
 
 USER="ubuntu"
 
-if [ "$#" -eq 4 ] && [ $3 = "--user" ]
+if [[ "$#" -eq 4 ]] && [[ $3 = "--user" ]]
 then
 	USER=$4
 fi
 
-if [ "$1" = "--hostfile" ]
+if [[ "$1" = "--hostfile" ]]
 then
 	while IFS=' ' read -r ADDRESS slots
 	do
-		if [ "$ADDRESS" != "localhost" ] && [ "$ADDRESS" != "127.0.0.1" ]
+		if [[ "$ADDRESS" != "localhost" ]] && [[ "$ADDRESS" != "127.0.0.1" ]]
 		then
 			echo "Copying files to $ADDRESS ..."
 			ssh -o StrictHostKeyChecking=no "$USER"@"$ADDRESS" "mkdir -p $PWD"
-			scp -r "$EXECUTABLE" "$DATADIR" "$USER"@"$ADDRESS":"$PWD"
+			scp -o StrictHostKeyChecking=no -r "$EXECUTABLE" "$DATADIR" "$USER"@"$ADDRESS":"$PWD"
 			echo
 		fi
 	done < "$2"
 
     echo "Files copied onto the slaves!"
-    exit
 else
     echo "Unknown option \""$1"\""
-    exit
 fi
+
+exit
